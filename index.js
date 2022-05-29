4cabcabbfdab9c09ec8acb9963cd5583e9f15409
HeiseMo <ttanurhan@DESKTOP-8NVOKRQ.localdomain>
2022-05-29 16:16:42 +0200

    dynamic values

--- index.js
+++ index.js
@@ -6,22 +6,11 @@ const Discord = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 const Gamedig = require('gamedig');
 const Rcon = require('rcon');
+const config = require('config');
+const fs = require('graceful-fs')
 
 
-//Connecto to RCON
-const conn = new Rcon(process.env.IP, process.env.PORT, process.env.RCONPASS);
-let rconRes = "";
-conn.on('auth', function() {
-  console.log("Authenticated");
-}).on('response', function(str) {
-  console.log("Response: " + str);
-  rconRes = str
-}).on('error', function(err) {
-  console.log("Error: " + err);
-}).on('end', function() {
-  console.log("Connection closed");
-  //process.exit();
-});
+
 
 // Create an instance of a Discord client
 const client = new Discord.Client({ intents: ["GUILD_PRESENCES", "GUILD_MEMBERS", "GUILDS", "GUILD_BANS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING", "GUILD_VOICE_STATES", "GUILD_MESSAGE_REACTIONS"] })
@@ -29,97 +18,182 @@ const client = new Discord.Client({ intents: ["GUILD_PRESENCES", "GUILD_MEMBERS"
 // Setup repeater
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 
-
 client.on('ready', async () => {
   console.log('I am ready!');
 });
 const prefix = "?";
 
+const ping = details =>
+  new Promise(resolve =>
+    setTimeout(() => resolve(details), 1000)
+  );
+
 setInterval(function() {
-  Gamedig.query({
-    type: '7d2d',
-    host: process.env.IP,
-    port: process.env.PORT
-  }).then((info) => {
-    console.log(info)
-    let myMemberCountChannel = client.channels.cache.get(process.env.ONLINEPLAYERCOUNTCHANNEL);
-    let myStatusChannel = client.channels.cache.get(process.env.SERVERSTATUSCHANNEL);
-    if(info.ping > 0){
-      myStatusChannel
-      .setName(`Game Server Status: ✅`)
-      .catch(console.error);
-    }
-    myMemberCountChannel
-    .setName(`Online Players: ${info.raw.numplayers}`)
-    .catch(console.error);
-    console.log("I just checked " + info.raw.numplayers + " players online")
-  }).catch((error) => {
-      let myMemberCountChannel = client.channels.cache.get(process.env.ONLINEPLAYERCOUNTCHANNEL);
-      let myStatusChannel = client.channels.cache.get(process.env.SERVERSTATUSCHANNEL);
-      myStatusChannel
-      .setName(`Game Server Status: ❌`)
-      myMemberCountChannel
-      .setName(`Online Players: 0`)
-      .catch(console.error);
-  });
+
+  fs.readFile('config/default.json', function (err, data) {
+    let json = JSON.parse(data)
+    console.log(json.length)
+   // json.forEach(item => {
+     for ( let j = 0; j < json.length; j++){
+       let test1 =  client.channels.cache.get(json[j].data.playercount.toString());
+       let test2 =  client.channels.cache.get(json[j].data.serverstatus.toString());
+       console.log(json[j].serverID)
+      sleep(120000)
+       Gamedig.query({
+        type: '7d2d',
+        host: json[j].data.ip,
+        port: json[j].data.port
+      }).then((info) => {
+        let myMemberCountChannel = test1
+        let myStatusChannel = test2
+        console.log(info.ping, "ping")
+            myStatusChannel
+            .setName(`Game Server Status: ✅`)
+            .then(updated => console.log(`Updated guild name to ${updated.name}`))
+            .catch(console.error);
+            myMemberCountChannel
+            .setName(`Online Players: ${info.raw.numplayers}`)
+            .then(updated => console.log(`Updated guild name to ${updated.name}`))
+            .catch(console.error);
+            console.log("I just checked " + info.raw.numplayers + " players online")
+      }).catch((error) => {
+          console.log(error)
+          let myMemberCountChannel = test1
+          let myStatusChannel = test2
+              myStatusChannel
+              .setName(`Game Server Status: ❌`)
+              .then(updated => console.log(`Updated guild name to ${updated.name}`))
+              .catch(console.error);
+              myMemberCountChannel
+              .setName(`Online Players: 0`)
+              .then(updated => console.log(`Updated guild name to ${updated.name}`))
+              .catch(console.error);
+      });
+     }
+    })
 }, process.env.DISCORDCHANNELNAMEREFRESHRATE);
 
 client.on('messageCreate', async (message) => {
   if(message.content.startsWith(prefix + "rconM") && message.member.roles.cache.some(role => role.name === process.env.ROLE)){
+    message.delete(1000);
+      let myArray = message.content.split(" ")
+      console.log(myArray)
       let rconMessage = message.content.slice(7)
       console.log(rconMessage)
+      let rconIP = myArray[1]
+      let rconPort = myArray[2]
+      let rconPassword = myArray[3]
+      let rconCMD = myArray.slice(4)
+      let rconString = rconCMD.join(" ")
+      //Connecto to RCON
+        const conn = new Rcon(rconIP, rconPort, rconPassword);
+        let rconRes = "";
+        conn.on('auth', function() {
+          console.log("Authenticated");
+        }).on('response', function(str) {
+          console.log("Response: " + str);
+          rconRes = str
+        }).on('error', function(err) {
+          console.log("Error: " + err);
+        }).on('end', function() {
+          console.log("Connection closed");
+        });
       conn.connect();
       await sleep(2000)
-      conn.send("announce " + rconMessage);
+      conn.send("announce " + rconString);
       await sleep(2000)
-      if(rconRes == ""){rconRes =  "'" + rconMessage + "' message has been successfully sent to the server!"} 
+      if(rconRes == ""){rconRes =  "'" + rconString + "' message has been successfully sent to the server!"} 
       message.channel.send(rconRes)
       rconRes = ""
       conn.disconnect();
   }
   if(message.content.startsWith(prefix + "rconA") && message.member.roles.cache.some(role => role.name === process.env.ROLE)){
+    message.delete(1000);
+    let myArray = message.content.split(" ")
+    console.log(myArray)
     let rconMessage = message.content.slice(7)
+    console.log(rconMessage)
+    let rconIP = myArray[1]
+    let rconPort = myArray[2]
+    let rconPassword = myArray[3]
+    let rconCMD = myArray.slice(4)
+    let rconString = rconCMD.join(" ")
+    //Connecto to RCON
+      const conn = new Rcon(rconIP, rconPort, rconPassword);
+      let rconRes = "";
+      conn.on('auth', function() {
+        console.log("Authenticated");
+      }).on('response', function(str) {
+        console.log("Response: " + str);
+        rconRes = str
+      }).on('error', function(err) {
+        console.log("Error: " + err);
+      }).on('end', function() {
+        console.log("Connection closed");
+      });
     conn.connect();
     await sleep(2000)
-    conn.send("announcerestart " + Number(rconMessage))
+    conn.send("announcerestart " + Number(rconString))
     await sleep(2000)
-    if(rconRes == ""){rconRes = "Server will restart in " + rconMessage + " minute, announcement was successfully sent to the server!"} 
+    if(rconRes == ""){rconRes = "Server will restart in " + rconString + " minute, announcement was successfully sent to the server!"} 
     message.channel.send(rconRes)
     rconRes = ""
     conn.disconnect();
 }
-if(message.content.startsWith(prefix + 'players')  && message.member.roles.cache.some(role => role.name === process.env.ROLE)) {
-  Gamedig.query({
-    type: '7d2d',
-    host: process.env.IP,
-    port: process.env.PORT
-  }).then((info) => {
-    const embed = new Discord.MessageEmbed()
-    .setTitle(`Player List`)
-    .setColor('#810e0e')
-    .setThumbnail(process.env.SERVERLOGO)
-     .setTimestamp()
-     info.players.forEach(array => {
-      embed.addField(array.name, "Level: " + array.raw.score);
-    }); 
-    message.channel.send({ embeds: [embed] })
-  }).catch((error) => {
-    console.log(error);
-  });
+if(message.content.startsWith(prefix + 'players') && message.member.roles.cache.some(role => role.name === process.env.ROLE)) {
+  message.delete(1000);
+  let serverID = message.guild.id;
+  fs.readFile('config/default.json', function (err, data) {
+    let json = JSON.parse(data)
+    console.log(json[0])
+    const isFound = json.some(element => {
+      if (element.serverID === serverID) {
+
+    Gamedig.query({
+      type: '7d2d',
+      host: element.data.ip,
+      port: element.data.port
+    }).then((info) => {
+      const embed = new Discord.MessageEmbed()
+      .setTitle(`Player List`)
+      .setColor('#810e0e')
+      .setThumbnail(element.data.serverlogo)
+       .setTimestamp()
+       info.players.forEach(array => {
+        embed.addFields({
+            name: array.name,
+           value: `Level: ${array.raw.score}`,
+            inline: true});
+      });
+
+      message.channel.send({ embeds: [embed] })
+    }).catch((error) => {
+      console.log(error);
+    });
+    }
+  })
+})
 }
 if(message.content.startsWith(prefix + 'admin') && message.member.roles.cache.some(role => role.name === process.env.ROLE)) {
+  message.delete(1000);
+  let serverID = message.guild.id;
+  fs.readFile('config/default.json', function (err, data) {
+    let json = JSON.parse(data)
+    console.log(json[0])
+    const isFound = json.some(element => {
+      if (element.serverID === serverID) {
     const embed = new Discord.MessageEmbed()
     .setTitle(`Admin Commands`)
     .setColor('#810e0e')
-    .setThumbnail(process.env.SERVERLOGO)
+    .setThumbnail(element.data.serverlogo)
     .addFields( 
     {
-       name: '?rconM [message]' ,
+       name: '?rconM [server IP] [RCON Port] [RCON PASSWORD] [message]' ,
        value: "Sends a system message to the server",
        inline: false,
      },
      {
-        name: '?rconA [minutes]' ,
+        name: '?rconA [server IP] [RCON Port] [RCON PASSWORD] [minutes in number]' ,
         value: "Sends a system message to the server regarding a server restarted in the minutes defined.",
         inline: false,
       },
@@ -131,50 +205,116 @@ if(message.content.startsWith(prefix + 'admin') && message.member.roles.cache.so
       },
       {
         name: '?players' ,
-        value: "Will recive a list of players currently playing on the server.",
+        value: "Will receive a list of players currently playing on the server.",
+        inline: false,
+      },
+      {
+        name: '?register [Server IP] [Server Port] [Server Logo] [Role] [Player Count Channel ID] [Server Status Channel ID]' ,
+        value: "For server logo please link a png or jpg, for role please type in the role you would like for super users to use admin commands",
         inline: false,
       },
 )
-    .setTimestamp()  
+    .setTimestamp()
     message.channel.send({ embeds: [embed] })
+      }
+    })
+  })  
+}
+if(message.content.startsWith(prefix + 'register')  && message.member.roles.cache.some(role => role.name === process.env.ROLE)) { //Need to check before admin is set how can admin run?
+  message.delete(1000);
+  let myArray = message.content.split(" ")
+  
+  let serverID = message.guild.id;
+  let myServerIP = myArray[1]
+  let myServerPort = myArray[2]
+  let serverlogo = myArray[3]
+  let myImportantRole = myArray[4]
+  let myPlayerCounterChannelId = myArray[5]
+  let myServerStatusChannelId = myArray[6]
+  let reg = {
+    serverID : serverID.toString(),
+    data : {
+      ip: myServerIP.toString(),
+      port: myServerPort.toString(),
+      serverlogo: serverlogo.toString(),
+      role: myImportantRole.toString(),
+      playercount: myPlayerCounterChannelId.toString(),
+      serverstatus: myServerStatusChannelId.toString(),
+    }
+  }
+  let newReg = JSON.stringify(reg)
+  fs.readFile('config/default.json', function (err, data) {
+    var json = JSON.parse(data)
+    let checkIfExists = json.findIndex(check => check.serverID == serverID.toString());
+    console.log(checkIfExists)
+    if(checkIfExists == -1){
+      console.log("I got in here because dont exist yet")
+      let parsedReg = JSON.parse(newReg)
+        json.push(parsedReg)
+        fs.writeFile("config/default.json", JSON.stringify(json))
+    } else {
+      console.log("I got in here because I exist already")
+      let parsedReg = JSON.parse(newReg)
+      json.splice(checkIfExists, 1);
+      console.log(json)
+      json.push(parsedReg)
+      fs.writeFile("config/default.json", JSON.stringify(json))
+
+    }
+
+})
+
 }
   if(message.content.startsWith(prefix + 'info')) {
-    Gamedig.query({
-      type: '7d2d',
-      host: process.env.IP,
-      port: process.env.PORT
-    }).then((info) => {
-      const embed = new Discord.MessageEmbed()
-      .setTitle(`Server Information`)
-      .setColor('#810e0e')
-      .setThumbnail(process.env.SERVERLOGO)
-      .addFields( 
-      {
-         name: 'Server Name' ,
-         value: info.name,
-         inline: false,
-       },
-       {
-          name: 'Server Ip' ,
-          value: info.connect,
-          inline: false,
-        },
-        {
-          name: 'Slots Available' ,
-          value: "" + (info.maxplayers - info.raw.numplayers),
-          inline: true,
-          
-        },
-        {
-          name: 'Online Players' ,
-          value: "" + info.raw.numplayers,
-          inline: true,
-        })
-      .setTimestamp()  
-      message.channel.send({ embeds: [embed] })
-    }).catch((error) => {
-      console.log(error);
-    });
+    message.delete(1000);
+    let serverID = message.guild.id;
+    fs.readFile('config/default.json', function (err, data) {
+      let json = JSON.parse(data)
+      console.log(json[0])
+      const isFound = json.some(element => {
+        if (element.serverID === serverID) {
+          Gamedig.query({
+            type: '7d2d',
+            host: element.data.ip,
+            port: element.data.port
+          }).then((info) => {
+            const embed = new Discord.MessageEmbed()
+            .setTitle(`Server Information`)
+            .setColor('#810e0e')
+            .setThumbnail(element.data.serverlogo)
+            .addFields( 
+            {
+               name: 'Server Name' ,
+               value: info.name,
+               inline: false,
+             },
+             {
+                name: 'Server Ip' ,
+                value: info.connect,
+                inline: false,
+              },
+              {
+                name: 'Slots Available' ,
+                value: "" + (info.maxplayers - info.raw.numplayers),
+                inline: true,
+                
+              },
+              {
+                name: 'Online Players' ,
+                value: "" + info.raw.numplayers,
+                inline: true,
+              })
+            .setTimestamp()  
+            message.channel.send({ embeds: [embed] })
+          }).catch((error) => {
+            console.log(error);
+          });
+          return true;
+        }
+        return false;
+      });
+      console.log(isFound)
+  })
   }
 });
 
